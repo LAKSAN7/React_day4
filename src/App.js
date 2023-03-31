3c46ac7cb3edbe64992f53c6e8ac6dffb116ee97
LAKSAN7 <laksanandpowerl@gmail.com>
2023-03-31 15:06:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+  import logo from './logo.svg';
+  import './App.css';
+  import FruitSurvey from './components/fruit';
+
+  function App() {
+    return (
+      <div>
+        <FruitSurvey />
+      </div>
+    );
+  }
+
+  export default App
---
+++ src/header.js
@@ -0,0 +1,12 @@
+import { AppBar } from '@material-ui/core'
+import React from 'react'
+
+export default function header() {
+  return (
+    <div>
+        <AppBar>
+            <h1>MY HEADER</h1>
+        </AppBar>
+    </div>
+  )
+}
